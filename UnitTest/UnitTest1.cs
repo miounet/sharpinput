0d7dde694b3ce3027b343aa315c50507831154ce
miounet <miounet@qq.com>
2024-11-29 13:30:34 +0800

    add

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -9,9 +9,7 @@ namespace UnitTest
         [TestMethod]
         public void TestMethod1()
         {
-            string ss = Core.Comm.Function.ConvertToChinese(109101231011.232);
-            var ll = Core.Comm.Function.ConverToDate("02");
-           string bb = ss;
+            
         }
     }
 }
